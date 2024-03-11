665f76ca4de544ad731060aebcd4e6df9cf8e7ea
codeiva4u <153196182+codeiva4u@users.noreply.github.com>
2024-03-11 23:25:36 +0530

    Update Hdmovie2.kt

--- Movierulzhd/src/main/kotlin/com/hexated/Hdmovie2.kt
+++ Movierulzhd/src/main/kotlin/com/hexated/Hdmovie2.kt
@@ -12,7 +12,7 @@ import org.jsoup.Jsoup
 
 class Hdmovie2 : Movierulzhd() {
 
-    override var mainUrl = "https://hdmovie2.chat/"
+    override var mainUrl = "https://hdmovie2.at"
     override var name = "Hdmovie2"
     override val mainPage = mainPageOf(
         "movies" to "New Release",
