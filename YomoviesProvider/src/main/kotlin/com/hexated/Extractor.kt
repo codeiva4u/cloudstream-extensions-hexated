d579775e55e92a002db7d1f8989dca4291f9b62c
codeiva4u <153196182+codeiva4u@users.noreply.github.com>
2024-07-02 20:15:04 +0530

    Update Extractor.kt

--- YomoviesProvider/src/main/kotlin/com/hexated/Extractor.kt
+++ YomoviesProvider/src/main/kotlin/com/hexated/Extractor.kt
@@ -5,8 +5,6 @@ import com.lagradost.cloudstream3.extractors.Minoplres
 open class Minoplres : Minoplres() {
 
     override val name = "Minoplres" // formerly SpeedoStream
-    override val requiresReferer = true
     override val mainUrl = "https://minoplres.xyz" // formerly speedostream.bond
-    private val hostUrl = "https://minoplres.xyz"
 
 }
