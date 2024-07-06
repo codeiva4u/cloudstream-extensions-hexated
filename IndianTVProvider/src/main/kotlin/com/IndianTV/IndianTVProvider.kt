855ea894903f447ad7bdc3353d3cf8adb35fe60c
codeiva4u <153196182+codeiva4u@users.noreply.github.com>
2024-07-07 00:11:12 +0530

    Update IndianTVProvider.kt

--- IndianTVProvider/src/main/kotlin/com/IndianTV/IndianTVProvider.kt
+++ IndianTVProvider/src/main/kotlin/com/IndianTV/IndianTVProvider.kt
@@ -6,9 +6,7 @@ import org.jsoup.nodes.Element
 import com.lagradost.cloudstream3.*
 import com.lagradost.cloudstream3.utils.*
 import com.lagradost.cloudstream3.utils.ExtractorLink
-import android.util.Base64
 import org.jsoup.nodes.Document
-import java.nio.charset.StandardCharsets
 
 class IndianTVProvider : MainAPI() {
     override var name = "Indian TV"
@@ -18,101 +16,81 @@ class IndianTVProvider : MainAPI() {
 
     override val mainPage = mainPageOf(
         INDIANTATAAPI to "TATA",
-        INDIANJIOAPI to "Jio TV",
-//        INDIANDiscoveryAPI to "Discovery",
-//        INDIANTVVootAPI to "Voot",
-//        INDIANAirtelAPI to "Airtel"
+        INDIANJIOAPI to "Jio TV"
     )
 
     companion object {
         const val INDIANJIOAPI = "https://madstream.one/pages/jiotv.php"
         const val INDIANTATAAPI = "https://madstream.one/pages/tataplay.php"
-//        const val INDIANDiscoveryAPI = BuildConfig.INDIANTV_Discovery_API
-//        const val INDIANAirtelAPI = BuildConfig.INDIANTV_Airtel_API
-//        const val INDIANTVVootAPI = BuildConfig.INDIANTV_Voot_API
 
         val Useragent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:125.0) Gecko/20100101 Firefox/125.0"
     }
 
     override suspend fun getMainPage(page: Int, request: MainPageRequest): HomePageResponse {
-        val document = app.get(request.data,headers = mapOf("User-Agent" to Useragent)).document
-            val home =
-                document.select("div#listContainer > div.box1").mapNotNull { it.toSearchResult() }
-            return newHomePageResponse(
-                list = HomePageList(
-                    name = request.name,
-                    list = home,
-                    isHorizontalImages = true
-                ),
-                hasNext = false
-            )
+        val document = app.get(request.data, headers = mapOf("User-Agent" to Useragent)).document
+        val home = document.select("div#listContainer > div.box1").mapNotNull { it.toSearchResult() }
+        return newHomePageResponse(
+            list = HomePageList(
+                name = request.name,
+                list = home,
+                isHorizontalImages = true
+            ),
+            hasNext = false
+        )
     }
+
     private fun Element.toSearchResult(): SearchResponse {
-            val title = this.select("h2.text-center").text()
-            val href = fixUrl(this.select("a").attr("href"))
-            val posterUrl = fixUrlNull(this.select("img").attr("src"))
-            //val category = this.select("p").text()
-            return newMovieSearchResponse(title, href, TvType.Live) {
-                this.posterUrl = posterUrl
-            }
+        val title = this.select("h2.text-center").text()
+        val href = fixUrl(this.select("a").attr("href"))
+        val posterUrl = fixUrlNull(this.select("img").attr("src"))
+        return newMovieSearchResponse(title, href, TvType.Live) {
+            this.posterUrl = posterUrl
+        }
     }
 
     override suspend fun search(query: String): List<SearchResponse> {
-        val documentTata = app.get(INDIANTATAAPI,headers = mapOf("User-Agent" to Useragent)).document
-   //     val documentAirtel = app.get(INDIANAirtelAPI,headers = mapOf("User-Agent" to Useragent)).document
-        val documentJiotv = app.get(INDIANJIOAPI,headers = mapOf("User-Agent" to Useragent)).document
-   //     val documentdiscovery = app.get(INDIANDiscoveryAPI,headers = mapOf("User-Agent" to Useragent)).document
+        val documentTata = app.get(INDIANTATAAPI, headers = mapOf("User-Agent" to Useragent)).document
+        val documentJiotv = app.get(INDIANJIOAPI, headers = mapOf("User-Agent" to Useragent)).document
         val mergedDocument = Document.createShell("")
         mergedDocument.body().append(documentTata.body().html())
-   //     mergedDocument.body().append(documentdiscovery.body().html())
-   //     mergedDocument.body().append(documentAirtel.body().html())
         mergedDocument.body().append(documentJiotv.body().html())
 
-            return mergedDocument.select("div#listContainer div.box1:contains($query), div#listContainer div.box1:contains($query)")
-                .mapNotNull {
-                    it.toSearchResult()
-                }
+        return mergedDocument.select("div#listContainer div.box1:contains($query)")
+            .mapNotNull { it.toSearchResult() }
     }
 
     override suspend fun load(url: String): LoadResponse {
-        if (url.contains("jiotv"))
-        {
-            val title ="JioTV"
-            val poster ="https://i0.wp.com/www.smartprix.com/bytes/wp-content/uploads/2021/08/JioTV-on-smart-TV.png?fit=1200%2C675&ssl=1"
-            val showname ="JioTV"
+        if (url.contains("jiotv")) {
+            val title = "JioTV"
+            val poster = "https://i0.wp.com/www.smartprix.com/bytes/wp-content/uploads/2021/08/JioTV-on-smart-TV.png?fit=1200%2C675&ssl=1"
+            val showname = "JioTV"
+
+            return newMovieLoadResponse(title, url, TvType.Live, url) {
+                this.posterUrl = poster
+                this.plot = showname
+            }
+        } else if (url.contains("tata")) {
+            val title = "TATA"
+            val poster = "https://cdn.mos.cms.futurecdn.net/iYdoTcTScdApk3JV5GfEAT-1920-80.jpg"
+            val showname = "TATA"
 
             return newMovieLoadResponse(title, url, TvType.Live, url) {
                 this.posterUrl = poster
                 this.plot = showname
             }
         }
-        else
-            if (url.contains("tata"))
-            {
-                val title ="TATA"
-                val poster ="https://cdn.mos.cms.futurecdn.net/iYdoTcTScdApk3JV5GfEAT-1920-80.jpg"
-                val showname ="TATA"
+        val document = app.get(url, headers = mapOf("User-Agent" to Useragent)).document
+        val title = document.selectFirst("div.program-info > span.channel-name")?.text()?.trim().toString()
+        val poster = "https://cdn.mos.cms.futurecdn.net/iYdoTcTScdApk3JV5GfEAT-1920-80.jpg"
+        val showname = document.selectFirst("div.program-info > div.program-name")?.text()?.trim().toString()
 
-                return newMovieLoadResponse(title, url, TvType.Live, url) {
-                    this.posterUrl = poster
-                    this.plot = showname
-                }
-            }
-            val document = app.get(url, headers = mapOf("User-Agent" to Useragent)).document
-            val title =document.selectFirst("div.program-info > span.channel-name")?.text()?.trim().toString()
-            val poster ="https://cdn.mos.cms.futurecdn.net/iYdoTcTScdApk3JV5GfEAT-1920-80.jpg"
-            val showname =document.selectFirst("div.program-info > div.program-name")?.text()?.trim().toString()
-            //val description = document.selectFirst("div.program-info > div.program-description")?.text()?.trim().toString()
-
-            return newMovieLoadResponse(title, url, TvType.Live, url) {
-                this.posterUrl = poster
-                this.plot = showname
-            }
+        return newMovieLoadResponse(title, url, TvType.Live, url) {
+            this.posterUrl = poster
+            this.plot = showname
+        }
     }
 
-    // Define a nullable global variable to store globalArgument
-
-  @SuppressLint("SuspiciousIndentation")
+    @SuppressLint("SuspiciousIndentation")
     override suspend fun loadLinks(
         data: String,
         isCasting: Boolean,
@@ -122,8 +100,8 @@ class IndianTVProvider : MainAPI() {
         Log.d("Phisher", data)
         when {
             data.contains("jiotv") -> {
-                val channelID = data.substringAfter("=")
-                val link = "https://madplay.live/hls/jiotv/stream.php?id=$channelID&e=.m3u8"
+                val channelID = data.substringAfter("play=")
+                val link = "https://madplay.live/hls/jiotv/?play=$channelID"
                 callback.invoke(
                     ExtractorLink(
                         source = "INDIAN TV",
@@ -135,7 +113,6 @@ class IndianTVProvider : MainAPI() {
                     )
                 )
             }
-
             data.contains("tata") -> {
                 val channelID = data.substringAfter("id=").substringBefore("&")
                 val link = "https://madplay.live/hls/tata/play.php?id=$channelID"
@@ -150,7 +127,6 @@ class IndianTVProvider : MainAPI() {
                     )
                 )
             }
-
             data.contains("jwplayer.php?") -> {
                 val link = data.substringAfter("jwplayer.php?")
                 callback.invoke(
