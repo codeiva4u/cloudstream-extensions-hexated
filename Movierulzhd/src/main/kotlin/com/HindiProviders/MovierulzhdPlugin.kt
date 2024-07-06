359949bd6ad943de1bf47c0cf820dbb3905b8834
codeiva4u <153196182+codeiva4u@users.noreply.github.com>
2024-07-06 23:17:20 +0530

    Add files via upload

---
+++ Movierulzhd/src/main/kotlin/com/HindiProviders/MovierulzhdPlugin.kt
@@ -0,0 +1,21 @@
+package com.HindiProviders
+
+import com.lagradost.cloudstream3.plugins.CloudstreamPlugin
+import com.lagradost.cloudstream3.plugins.Plugin
+import android.content.Context
+import com.HindiProviders.Akamaicdn
+import com.HindiProviders.FMHD
+import com.HindiProviders.Hdmovie2
+import com.HindiProviders.Movierulzhd
+import com.lagradost.cloudstream3.extractors.VidSrcTo
+
+@CloudstreamPlugin
+class MovierulzhdPlugin: Plugin() {
+    override fun load(context: Context) {
+        // All providers should be added in this manner. Please don't edit the providers list directly.
+        registerMainAPI(Movierulzhd())
+        registerExtractorAPI(FMHD())
+        registerExtractorAPI(VidSrcTo())
+        registerExtractorAPI(Akamaicdn())
+    }
+}
