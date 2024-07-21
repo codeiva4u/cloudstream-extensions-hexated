bc4ad7eddd3eb99f779fdfa2f51636026bae7a86
codeiva4u <153196182+codeiva4u@users.noreply.github.com>
2024-07-21 18:34:06 +0530

    Update Extractors.kt

--- MoviesDrive/src/main/kotlin/com/megix/Extractors.kt
+++ MoviesDrive/src/main/kotlin/com/megix/Extractors.kt
@@ -17,12 +17,11 @@ class HubCloud : ExtractorApi() {
         val doc = app.get(url).text
         val newLink = doc.substringAfter("url=").substringBefore("\"")
         val newDoc = app.get(newLink).document
-        var gamerLink: String
+        var gamerLink = ""
 
         if(newLink.contains("drive")) {
-            val scriptTag = newDoc.selectFirst("script:containsData(url)")?.toString()
-            gamerLink = scriptTag?.let { Regex("var url = '([^']*)'").find(it) ?. groupValues ?. get(1) }
-                ?: ""
+            val scriptTag = newDoc.selectFirst("script:containsData(url)").toString()
+            gamerLink = Regex("var url = '([^']*)'").find(scriptTag) ?. groupValues ?. get(1) ?: ""
         }
 
         else {
@@ -34,51 +33,45 @@ class HubCloud : ExtractorApi() {
         val size = document.selectFirst("i#size") ?. text()
         val div = document.selectFirst("div.card-body")
         val header = document.selectFirst("div.card-header") ?. text()
-        if (div != null) {
-            div.select("a").apmap {
-                val link = it.attr("href")
-                if (link.contains("pixeldra")) {
-                    callback.invoke(
-                        ExtractorLink(
-                            "Pixeldrain",
-                            "Pixeldrain $size",
-                            link,
-                            "",
-                            getIndexQuality(header),
-                        )
+        div.select("a").apmap {
+            val link = it.attr("href")
+            if (link.contains("pixeldra")) {
+                callback.invoke(
+                    ExtractorLink(
+                        "Pixeldrain",
+                        "Pixeldrain $size",
+                        link,
+                        "",
+                        getIndexQuality(header),
                     )
-                }
-                else if(link.contains("dl.php")) {
-                    val downloadPage = app.get(link).document
-                    val downloadLink = downloadPage.selectFirst("a#vd")?.attr("href")
-                    downloadLink?.let { it1 ->
-                        ExtractorLink(
-                            "Hub-Cloud[Download]",
-                            "Hub-Cloud[Download] $size",
-                            it1,
-                            "",
-                            getIndexQuality(header),
-                        )
-                    }?.let { it2 ->
-                        callback.invoke(
-                            it2
-                        )
-                    }
-                }
-                else if(link.contains(".dev")) {
-                    callback.invoke(
-                        ExtractorLink(
-                            "Hub-Cloud",
-                            "Hub-Cloud $size",
-                            link,
-                            "",
-                            getIndexQuality(header),
-                        )
+                )
+            }
+            else if(link.contains("dl.php")) {
+                val response = app.get(link, allowRedirects = false)
+                val downloadLink = response.headers["location"].toString().split("link=").getOrNull(1) ?: link
+                callback.invoke(
+                    ExtractorLink(
+                        "Hub-Cloud[Download]",
+                        "Hub-Cloud[Download] $size",
+                        downloadLink,
+                        "",
+                        getIndexQuality(header),
                     )
-                }
-                else {
-                    //loadExtractor(link, subtitleCallback, callback)
-                }
+                )
+            }
+            else if(link.contains(".dev")) {
+                callback.invoke(
+                    ExtractorLink(
+                        "Hub-Cloud",
+                        "Hub-Cloud $size",
+                        link,
+                        "",
+                        getIndexQuality(header),
+                    )
+                )
+            }
+            else {
+                loadExtractor(link, subtitleCallback, callback)
             }
         }
     }
