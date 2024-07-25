29a5b34e12de1ea5ad960f3d06b938ad71fcdbf1
codeiva4u <153196182+codeiva4u@users.noreply.github.com>
2024-07-25 21:11:48 +0530

    Update Extractor.kt

--- MultiMoviesProvider/src/main/kotlin/com/HindiProviders/Extractor.kt
+++ MultiMoviesProvider/src/main/kotlin/com/HindiProviders/Extractor.kt
@@ -23,7 +23,7 @@ class MultimoviesAIO: StreamWishExtractor() {
 
 class Asnwish : StreamWishExtractor() {
     override var name = "Multimovies StreamWish Asnwish"
-    override var mainUrl = "https://snapinsta.storyy.shop"
+    override var mainUrl = "https://cdnwish.com"
 }
 class Animezia : VidhideExtractor() {
     override var name = "Animezia"
