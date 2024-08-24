601b1fa7bb55dd9431604e3c5289706726d2ff88
codeiva4u <153196182+codeiva4u@users.noreply.github.com>
2024-08-24 10:22:33 +0530

    Update MovierulzhdPlugin.kt

--- Movierulzhd/src/main/kotlin/com/HindiProviders/MovierulzhdPlugin.kt
+++ Movierulzhd/src/main/kotlin/com/HindiProviders/MovierulzhdPlugin.kt
@@ -10,11 +10,12 @@ class MovierulzhdPlugin: Plugin() {
     override fun load(context: Context) {
         // All providers should be added in this manner. Please don't edit the providers list directly.
         registerMainAPI(Movierulzhd())
-    //    registerMainAPI(Hdmovie2())
+     //   registerMainAPI(Hdmovie2())
         registerExtractorAPI(FMHD())
         registerExtractorAPI(VidSrcTo())
         registerExtractorAPI(Akamaicdn())
         registerExtractorAPI(Luluvdo())
         registerExtractorAPI(FMX())
+        registerExtractorAPI(Lulust())
     }
 }
