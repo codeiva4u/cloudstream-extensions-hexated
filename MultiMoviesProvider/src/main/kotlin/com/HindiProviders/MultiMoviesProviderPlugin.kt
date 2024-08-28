c52b6b679ad9bce26f8ce539a3d50eaea0880854
codeiva4u <153196182+codeiva4u@users.noreply.github.com>
2024-08-28 23:11:37 +0530

    Update MultiMoviesProviderPlugin.kt

--- MultiMoviesProvider/src/main/kotlin/com/HindiProviders/MultiMoviesProviderPlugin.kt
+++ MultiMoviesProvider/src/main/kotlin/com/HindiProviders/MultiMoviesProviderPlugin.kt
@@ -1,13 +1,12 @@
 package com.HindiProviders
 
-import android.content.Context
-import com.lagradost.cloudstream3.extractors.Chillx
-import com.lagradost.cloudstream3.extractors.DoodLaExtractor
-import com.lagradost.cloudstream3.extractors.MixDrop
-import com.lagradost.cloudstream3.extractors.StreamTape
-import com.lagradost.cloudstream3.extractors.XStreamCdn
 import com.lagradost.cloudstream3.plugins.CloudstreamPlugin
 import com.lagradost.cloudstream3.plugins.Plugin
+import android.content.Context
+import com.lagradost.cloudstream3.extractors.DoodLaExtractor
+import com.lagradost.cloudstream3.extractors.MixDrop
+import com.lagradost.cloudstream3.extractors.XStreamCdn
+import com.lagradost.cloudstream3.extractors.CdnwishCom
 
 @CloudstreamPlugin
 class MultiMoviesProviderPlugin: Plugin() {
@@ -15,27 +14,15 @@ class MultiMoviesProviderPlugin: Plugin() {
         // All providers should be added in this manner. Please don't edit the providers list directly.
         registerMainAPI(MultiMoviesProvider())
         registerExtractorAPI(MixDrop())
+        registerExtractorAPI(Multimovies())
         registerExtractorAPI(XStreamCdn())
         registerExtractorAPI(DoodLaExtractor())
-        registerExtractorAPI(StreamTape())
-        registerExtractorAPI(Chillx())
-        registerExtractorAPI(MultimoviesAIO())
-        registerExtractorAPI(Multimovies())
         registerExtractorAPI(Animezia())
         registerExtractorAPI(server2())
+        registerExtractorAPI(MultimoviesAIO())
         registerExtractorAPI(GDMirrorbot())
-        registerExtractorAPI(VidHidePro())
-        registerExtractorAPI(filelions())
-        registerExtractorAPI(filelionslive())
-        registerExtractorAPI(VidHidePro3())
-        registerExtractorAPI(VidHidePro4())
-        registerExtractorAPI(VidHidePro5())
-        registerExtractorAPI(VidHidePro6())
-        registerExtractorAPI(Lulustream1())
-        registerExtractorAPI(Lulustream2())
-        registerExtractorAPI(Lulustream3())
-       
-
-
+        registerExtractorAPI(VidhideExtractor())
+        registerExtractorAPI(CdnwishCom())
+        
     }
 }
