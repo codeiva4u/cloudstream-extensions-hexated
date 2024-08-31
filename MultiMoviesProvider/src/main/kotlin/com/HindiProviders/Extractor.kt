c0329722d26dba2cd932b27a1b364af791c9946f
codeiva4u <153196182+codeiva4u@users.noreply.github.com>
2024-08-31 22:25:28 +0530

    Update Extractor.kt

--- MultiMoviesProvider/src/main/kotlin/com/HindiProviders/Extractor.kt
+++ MultiMoviesProvider/src/main/kotlin/com/HindiProviders/Extractor.kt
@@ -32,42 +32,16 @@ class server2 : VidhideExtractor() {
 }
 
 class Asnwish : StreamWishExtractor() {
-    override val name = "Streanwish Asn"
+    override var name = "Streanwish Asn"
     override val mainUrl = "https://asnwish.com"
     override val requiresReferer = true
 }
 
-class CdnwishCom : StreamWishExtractor() {
-    override val name = "Cdnwish"
-    override val mainUrl = "https://cdnwish.com"
-    override val requiresReferer = true
-}
-/*
-class GDMirrorbot : ExtractorApi() {
-    override var name = "GDMirrorbot"
-    override var mainUrl = "https://gdmirrorbot.nl"
-    override val requiresReferer = true
-    override suspend fun getUrl(
-        url: String,
-        referer: String?,
-        subtitleCallback: (SubtitleFile) -> Unit,
-        callback: (ExtractorLink) -> Unit) {
-        Log.d("Phisher url","$url")
-        app.get(url).document.select("ul#videoLinks li").map {
-            val link=it.attr("data-link")
-            Log.d("Phisher url","$link")
-            loadExtractor(link,"https://multimovies.sbs",subtitleCallback, callback)
-        }
-    }
-}
- */
 class Strwishcom : StreamWishExtractor() {
-    override val name = "Strwish"
+    override var name = "Strwish"
     override val mainUrl = "https://strwish.com"
     override val requiresReferer = true
 }
-
-
 
 open class VidhideExtractor : ExtractorApi() {
     override var name = "VidHide"
