f8b08aaefd1d7074cf9ab2a128d5e1efffd1037f
codeiva4u <153196182+codeiva4u@users.noreply.github.com>
2024-09-01 10:52:29 +0530

    Update MultiMoviesProviderPlugin.kt

--- MultiMoviesProvider/src/main/kotlin/com/HindiProviders/MultiMoviesProviderPlugin.kt
+++ MultiMoviesProvider/src/main/kotlin/com/HindiProviders/MultiMoviesProviderPlugin.kt
@@ -1,65 +1,32 @@
 package com.HindiProviders
 
-import android.content.Context
-import com.lagradost.cloudstream3.extractors.AStreamHub
-import com.lagradost.cloudstream3.extractors.Acefile
-import com.lagradost.cloudstream3.extractors.Ahvsh
-import com.lagradost.cloudstream3.extractors.Aico
-import com.lagradost.cloudstream3.extractors.Bestx
-import com.lagradost.cloudstream3.extractors.BullStream
-import com.lagradost.cloudstream3.extractors.Cdnplayer
-import com.lagradost.cloudstream3.extractors.CdnwishCom
-import com.lagradost.cloudstream3.extractors.Chillx
-import com.lagradost.cloudstream3.extractors.DBfilm
-import com.lagradost.cloudstream3.extractors.DoodLaExtractor
-import com.lagradost.cloudstream3.extractors.FEmbed
-import com.lagradost.cloudstream3.extractors.FileMoon
-import com.lagradost.cloudstream3.extractors.GMPlayer
-import com.lagradost.cloudstream3.extractors.Gdriveplayer
-import com.lagradost.cloudstream3.extractors.GenericM3U8
-import com.lagradost.cloudstream3.extractors.HDMomPlayer
-import com.lagradost.cloudstream3.extractors.JWPlayer
-import com.lagradost.cloudstream3.extractors.Minoplres
-import com.lagradost.cloudstream3.extractors.MixDrop
-import com.lagradost.cloudstream3.extractors.StreamTape
-import com.lagradost.cloudstream3.extractors.StreamWishExtractor
-import com.lagradost.cloudstream3.extractors.XStreamCdn
 import com.lagradost.cloudstream3.plugins.CloudstreamPlugin
 import com.lagradost.cloudstream3.plugins.Plugin
+import android.content.Context
+import com.lagradost.cloudstream3.extractors.DoodLaExtractor
+import com.lagradost.cloudstream3.extractors.MixDrop
+import com.lagradost.cloudstream3.extractors.XStreamCdn
+import com.lagradost.cloudstream3.extractors.VidHidePro5
+import com.lagradost.cloudstream3.extractors.VidHidePro6
 
 @CloudstreamPlugin
 class MultiMoviesProviderPlugin: Plugin() {
     override fun load(context: Context) {
         // All providers should be added in this manner. Please don't edit the providers list directly.
         registerMainAPI(MultiMoviesProvider())
+        registerExtractorAPI(VidHidePro5())
         registerExtractorAPI(MixDrop())
+        registerExtractorAPI(Multimovies())
         registerExtractorAPI(XStreamCdn())
         registerExtractorAPI(DoodLaExtractor())
-        registerExtractorAPI(StreamTape())
-        registerExtractorAPI(Chillx())
-        registerExtractorAPI(AStreamHub())
-        registerExtractorAPI(Acefile())
-        registerExtractorAPI(Ahvsh())
-        registerExtractorAPI(Aico())
-        registerExtractorAPI(Bestx())
-        registerExtractorAPI(BullStream())
-        registerExtractorAPI(Cdnplayer())
-        registerExtractorAPI(DBfilm())
-        registerExtractorAPI(FEmbed())
-        registerExtractorAPI(FileMoon())
-        registerExtractorAPI(GMPlayer())
-        registerExtractorAPI(Gdriveplayer())
-        registerExtractorAPI(GenericM3U8())
-        registerExtractorAPI(HDMomPlayer())
-        registerExtractorAPI(JWPlayer())
-        registerExtractorAPI(Minoplres())
-        registerExtractorAPI(StreamWishExtractor())
-        registerExtractorAPI(MultimoviesAIO())
-        registerExtractorAPI(Multimovies())
         registerExtractorAPI(Animezia())
+        registerExtractorAPI(server2())
+        registerExtractorAPI(MultimoviesAIO())
+        //registerExtractorAPI(GDMirrorbot())
+        //registerExtractorAPI(VidhideExtractor())
         registerExtractorAPI(Asnwish())
-        registerExtractorAPI(Strwishcom())
         registerExtractorAPI(CdnwishCom())
-
-    }
+        registerExtractorAPI(Strwishcom())
+        registerExtractorAPI(VidHidePro6())
+        }
 }
