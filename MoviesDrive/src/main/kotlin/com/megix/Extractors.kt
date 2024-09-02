bf5479ef94faf3902e71e649467bc31d443f15f7
codeiva4u <153196182+codeiva4u@users.noreply.github.com>
2024-09-02 20:46:07 +0530

    Update Extractors.kt

--- MoviesDrive/src/main/kotlin/com/megix/Extractors.kt
+++ MoviesDrive/src/main/kotlin/com/megix/Extractors.kt
@@ -14,71 +14,71 @@ class HubCloud : ExtractorApi() {
         subtitleCallback: (SubtitleFile) -> Unit,
         callback: (ExtractorLink) -> Unit
     ) {
-        val doc = app.get(url).text
-        val newLink = doc.substringAfter("url=").substringBefore("\"")
+        val text = app.get(url).text
+        val newLink = text.substringAfter("url=").substringBefore("\"")
         val newDoc = app.get(newLink).document
-        val gamerLink: String
+        var gamerLink : String
 
-        gamerLink = if (newLink.contains("drive")) {
-            val scriptTag = newDoc.selectFirst("script:containsData(url)")?.toString()
-            scriptTag?.let { Regex("var url = '([^']*)'").find(it)?.groupValues?.get(1) } ?: ""
-        } else {
-            newDoc.selectFirst("div.vd > center > a")?.attr("href") ?: ""
+        if(newLink.contains("drive")) {
+            val scriptTag = newDoc.selectFirst("script:containsData(url)")?.toString() ?: ""
+            gamerLink = Regex("var url = '([^']*)'").find(scriptTag) ?. groupValues ?. get(1) ?: ""
+        }
+
+        else {
+            gamerLink = newDoc.selectFirst("div.vd > center > a") ?. attr("href") ?: ""
         }
 
         val document = app.get(gamerLink).document
-        val size = document.selectFirst("i#size")?.text()
+        val size = document.selectFirst("i#size") ?. text()
         val div = document.selectFirst("div.card-body")
-        val header = document.selectFirst("div.card-header")?.text()
-
-        div?.select("a")?.apmap {
+        val header = document.selectFirst("div.card-header") ?. text()
+        div?.select("a")?.amap {
             val link = it.attr("href")
-            val text = it.text()
-            when {
-                link.contains("pixeldra") -> {
-                    callback.invoke(
-                        ExtractorLink(
-                            "Pixeldrain",
-                            "Pixeldrain $size",
-                            link,
-                            "",
-                            getIndexQuality(header),
-                        )
+            if (link.contains("pixeldra")) {
+                callback.invoke(
+                    ExtractorLink(
+                        "Pixeldrain",
+                        "Pixeldrain $size",
+                        link,
+                        "",
+                        getIndexQuality(header),
                     )
-                }
-                text.contains("Download [Server : 10Gbps]") -> {
-                    val response = app.get(link, allowRedirects = false)
-                    val downloadLink = response.headers["location"].toString().split("link=").getOrNull(1) ?: link
-                    callback.invoke(
-                        ExtractorLink(
-                            "Hub-Cloud[Download]",
-                            "Hub-Cloud[Download] $size",
-                            downloadLink,
-                            "",
-                            getIndexQuality(header),
-                        )
+                )
+            }
+            else if(it.text().contains("Download [Server : 10Gbps]")) {
+                val response = app.get(link, allowRedirects = false)
+                val downloadLink = response.headers["location"].toString().split("link=").getOrNull(1) ?: link
+                callback.invoke(
+                    ExtractorLink(
+                        "Hub-Cloud[Download]",
+                        "Hub-Cloud[Download] $size",
+                        downloadLink,
+                        "",
+                        getIndexQuality(header),
                     )
-                }
-                link.contains(".dev") -> {
-                    callback.invoke(
-                        ExtractorLink(
-                            "Hub-Cloud",
-                            "Hub-Cloud $size",
-                            link,
-                            "",
-                            getIndexQuality(header),
-                        )
+                )
+            }
+            else if(link.contains(".dev")) {
+                callback.invoke(
+                    ExtractorLink(
+                        "Hub-Cloud",
+                        "Hub-Cloud $size",
+                        link,
+                        "",
+                        getIndexQuality(header),
                     )
-                }
-                else -> {
-                    loadExtractor(link, subtitleCallback, callback)
-                }
+                )
+            }
+            else {
+                loadExtractor(link, subtitleCallback, callback)
             }
         }
     }
 
+
     private fun getIndexQuality(str: String?): Int {
-        return Regex("(\\d{3,4})[pP]").find(str ?: "")?.groupValues?.getOrNull(1)?.toIntOrNull()
+        return Regex("(\\d{3,4})[pP]").find(str ?: "") ?. groupValues ?. getOrNull(1) ?. toIntOrNull()
             ?: Qualities.Unknown.value
     }
+
 }
